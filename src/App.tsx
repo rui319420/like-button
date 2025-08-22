2a67df0eb19b052bf44f4c1ce5a000dfdd92f5aa
Rui <s1F102501866@iniad.org>
2025-08-23 02:58:12 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <LikeButton />
+    </>
+  );
+}
+
+function LikeButton(){
+  return <span>いいねボタン予定地</span>;
+}
+
+export default App;
